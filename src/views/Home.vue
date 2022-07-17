a9400cc61852335b96784da3e5e5b830e850e47e
Youchul Kim <youchul@Youchului-Mac-Studio.local>
2022-07-17 20:08:15 +0900

    icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,30 +2,30 @@
   <PatchMeta
     :title="section ? section : 'Minimal Vue3 + Markdown blog engine'"
   />
-  <div class="max-w-5xl mx-auto px-4 sm:px-6 md:px-8">
+  <div class="max-w-5xl px-4 mx-auto sm:px-6 md:px-8">
     <div class="container px-4 mx-auto mt-8">
       <!-- HEADER -->
       <BlogHeader class="mb-5" />
 
       <hr v-if="section" />
-      <p v-if="section" class="text-center display-4 text-capitalize my-5">
+      <p v-if="section" class="my-5 text-center display-4 text-capitalize">
         {{ section }}
       </p>
 
       <div
         v-for="entry in pageStatus.visiblePosts"
         :key="entry.id"
-        class="rounded-sm border p-5 mb-6 shadow-sm w-full bg-white"
+        class="w-full p-5 mb-6 bg-white border rounded-sm shadow-sm"
       >
-        <div class="flex w-full items-center justify-between pb-1">
+        <div class="flex items-center justify-between w-full pb-1">
           <div class="flex items-center space-x-3">
             <div
-              class="h-8 w-8 min-w-[32px] rounded-full bg-slate-400 bg-cover bg-center"
+              class="h-8 w-8 min-w-[32px] rounded-md bg-contain bg-center border-none"
               :style="{
-                backgroundImage: `url('${REPO_ASSET_URL}${entry.section}-logo.png')`,
+                backgroundImage: `url('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${entry.section}/${entry.section}-original.svg')`,
               }"
             ></div>
-            <div class="sm:text-xl text-sm font-normal text-slate-700">
+            <div class="text-sm font-normal sm:text-xl text-slate-700">
               <router-link
                 :to="`/${entry.section}/${entry.id}`"
                 class="text-reset"
@@ -41,7 +41,7 @@
               class="text-reset"
             >
               <button
-                class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold hidden sm:inline-block"
+                class="hidden px-3 py-1 text-xs font-semibold border rounded-2xl bg-neutral-100 sm:inline-block"
               >
                 {{ entry.section }}
               </button>
@@ -60,11 +60,11 @@
       <!-- PAGINATION -->
       <ul
         v-if="pageStatus.endPage > pageStatus.startPage"
-        class="flex pl-0 list-none rounded my-2 pagination"
+        class="flex pl-0 my-2 list-none rounded pagination"
         style="cursor: pointer"
       >
         <li
-          class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-700 border-r-0 ml-0 rounded-l hover:bg-gray-200"
+          class="relative block px-3 py-2 ml-0 leading-tight text-gray-700 bg-white border border-r-0 border-gray-300 rounded-l hover:bg-gray-200"
           :class="currentPage == pageStatus.startPage ? 'active' : ''"
           @click="currentPage = pageStatus.startPage"
         >
@@ -73,14 +73,14 @@
         <li
           v-for="(page, index) in pageStatus.midPages"
           :key="index"
-          class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-700 border-r-0 hover:bg-gray-200"
+          class="relative block px-3 py-2 leading-tight text-gray-700 bg-white border border-r-0 border-gray-300 hover:bg-gray-200"
           :class="currentPage == page ? 'active' : ''"
           @click="currentPage = page"
         >
           <a class="page-link">{{ page }}</a>
         </li>
         <li
-          class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-700 rounded-r hover:bg-gray-200"
+          class="relative block px-3 py-2 leading-tight text-gray-700 bg-white border border-gray-300 rounded-r hover:bg-gray-200"
           :class="currentPage == pageStatus.endPage ? 'active' : ''"
           @click="currentPage = pageStatus.endPage"
         >
