741dd36f578c446847d19a91b7f9d386f1d96eca
Youchul Kim <youchul@apprz.net>
2022-07-18 14:10:37 +0900

    line 42

--- scripts/generate_index.ts
+++ scripts/generate_index.ts
@@ -1,32 +1,34 @@
-import { join, resolve } from'path'
-import removeMarkdown from 'markdown-to-text'
-import { writeFileSync, readdirSync, readFileSync } from 'fs'
-import { PostIndex } from '../src/types/PostIndex'
+import { join, resolve } from "path";
+import removeMarkdown from "markdown-to-text";
+import { writeFileSync, readdirSync, readFileSync } from "fs";
+import { PostIndex } from "../src/types/PostIndex";
 
-const dataPath = resolve('public', 'blog_store')
-const postsPath = join(dataPath, 'posts')
+const dataPath = resolve("public", "blog_store");
+const postsPath = join(dataPath, "posts");
 
 // Get categories
-const categoryFolders = readdirSync(postsPath)
+const categoryFolders = readdirSync(postsPath);
 
 // Create posts_index object
-const postsIndex: PostIndex[] = []
+const postsIndex: PostIndex[] = [];
 
 for (const categoryFolder of categoryFolders) {
-  const categoryPath = join(postsPath, categoryFolder)
-  const postFiles = readdirSync(categoryPath)
+  const categoryPath = join(postsPath, categoryFolder);
+  const postFiles = readdirSync(categoryPath);
 
   for (const postFile of postFiles) {
-    const postPath = join(categoryPath, postFile)
-    const postContent = readFileSync(postPath).toString()
+    const postPath = join(categoryPath, postFile);
+    const postContent = readFileSync(postPath).toString();
     const postLines = removeMarkdown(postContent)
-      .split('\n').map((l: String) => l.trim()).filter((l: String) => Boolean(l))
+      .split("\n")
+      .map((l: String) => l.trim())
+      .filter((l: String) => Boolean(l));
 
-    const [title, date, ...fullPost] = postLines
-    const fullPostLines = fullPost.join(' ')
-    const descriptionLines = fullPostLines.split(' ').slice(0, 64).join(' ')
-    const description = `${descriptionLines}${fullPostLines.trim().length > descriptionLines.trim().length ? '...' : ''}`
-    const [postFileId,] = postFile.split('.md')
+    const [title, date, ...fullPost] = postLines;
+    const fullPostLines = fullPost.join(" ");
+    const descriptionLines = fullPostLines.split(" ").slice(0, 42).join(" ");
+    const description = `${descriptionLines}${fullPostLines.trim().length > descriptionLines.trim().length ? "..." : ""}`;
+    const [postFileId] = postFile.split(".md");
 
     postsIndex.push({
       id: postFileId,
@@ -35,12 +37,12 @@ for (const categoryFolder of categoryFolders) {
       title,
       description,
       url: `blog_store/posts/${categoryFolder}/${postFile}`,
-    })
+    });
   }
 }
 
 // Sort Posts by date
-postsIndex.sort((a, b) => Date.parse(b.date) - Date.parse(a.date))
+postsIndex.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
 
-const indexPath = join(dataPath, 'posts_index.json')
-writeFileSync(indexPath, JSON.stringify(postsIndex, null, 4))
+const indexPath = join(dataPath, "posts_index.json");
+writeFileSync(indexPath, JSON.stringify(postsIndex, null, 4));
